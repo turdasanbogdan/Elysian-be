c130ecd20b378b2d4ca69aac6ba8764345ebbc39
turdasanbogdan <48791926+turdasanbogdan@users.noreply.github.com>
2021-02-01 03:18:02 +0200

    changes to allow CORS-Origin requests

--- src/main/java/turdasan/elysian/interview/ElysianBeApplication.java
+++ src/main/java/turdasan/elysian/interview/ElysianBeApplication.java
@@ -3,12 +3,23 @@ package turdasan.elysian.interview;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication()
 public class ElysianBeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ElysianBeApplication.class, args);
+    }
+    @Bean
+    public FilterRegistrationBean corsFilterRegistration() {
+        FilterRegistrationBean registrationBean =
+                new FilterRegistrationBean(new CORSFilter());
+        registrationBean.setName("CORS Filter");
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(1);
+        return registrationBean;
     }
 
 }
--- src/main/java/turdasan/elysian/interview/controller/PharmaciesRestController.java
+++ src/main/java/turdasan/elysian/interview/controller/PharmaciesRestController.java
@@ -1,10 +1,7 @@
 package turdasan.elysian.interview.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import turdasan.elysian.interview.entity.Pharmacy;
 import turdasan.elysian.interview.service.PharmaciesService;
 
@@ -42,6 +39,7 @@ public class PharmaciesRestController {
         return pharmacy;
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/city/{city}")
     public List<Pharmacy> findByCity(@PathVariable String city){
 
